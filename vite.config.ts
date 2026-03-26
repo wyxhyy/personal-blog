2c2180a4a654502dffadbaa5096cfd4e2ca559af
wyxhyy <2505948035@qq.com>
2026-03-26 17:23:27 +0800

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/personal-blog/',  // 注意：仓库名必须与你的 GitHub 仓库名称一致，以斜杠开头和结尾
 })
